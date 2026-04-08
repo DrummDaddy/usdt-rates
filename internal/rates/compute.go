9c3f0a1073603df703b15b11bdd43dc71f438e1c
DrummDaddy <alexpospelov87@gmail.com>
2026-04-08 17:30:32 +0700

    add compute and test

--- internal/rates/compute.go
+++ internal/rates/compute.go
@@ -1,1 +1,44 @@
 package rates
+
+import (
+	"fmt"
+
+	"github.com/shopspring/decimal"
+)
+
+func TopN(values []decimal.Decimal, n int) (decimal.Decimal, error) {
+	if n < 1 {
+		return decimal.Zero, fmt.Errorf(`n must be greater than zero`)
+
+	}
+	if n > len(values) {
+		return decimal.Zero, fmt.Errorf(`n=%d must be greater or equal to length=%d of values`, n, len(values))
+
+	}
+	return values[n-1], nil
+
+}
+
+func AvgNM(values []decimal.Decimal, n, m int) (decimal.Decimal, error) {
+	if n < 1 || m < 1 {
+		return decimal.Zero, fmt.Errorf(`n must be greater or equal to zero`)
+	}
+	if n > m {
+		return decimal.Zero, fmt.Errorf("n=%d must be greater or equal to m=%d", n, m)
+
+	}
+
+	if m > len(values) {
+		return decimal.Zero, fmt.Errorf("m=%d must be greater or equal to length=%d", m, len(values))
+	}
+
+	sub := values[n-1 : m]
+
+	sum := decimal.Zero
+	for _, v := range sub {
+		sum = sum.Add(v)
+	}
+	avg := sum.Div(decimal.NewFromInt(int64(len(sub))))
+	return avg, nil
+
+}
---
+++ internal/rates/compute_test.go
@@ -0,0 +1,31 @@
+package rates
+
+import (
+	"testing"
+
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTopN(t *testing.T) {
+	values := []decimal.Decimal{
+		decimal.NewFromInt(10),
+		decimal.NewFromInt(20),
+		decimal.NewFromInt(30),
+	}
+	v, err := TopN(values, 2)
+	require.NoError(t, err)
+	require.Equal(t, decimal.NewFromInt(20), v)
+}
+
+func TestAvgNM(t *testing.T) {
+	values := []decimal.Decimal{
+		decimal.NewFromInt(10),
+		decimal.NewFromInt(20),
+		decimal.NewFromInt(30),
+		decimal.NewFromInt(40),
+	}
+	avg, err := AvgNM(values, 2, 4)
+	require.NoError(t, err)
+	require.Equal(t, decimal.NewFromInt(40), avg)
+}
